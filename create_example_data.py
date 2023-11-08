883d3d0d9cd2c19ec23ae44c37b701a9ee993685
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-11-08 10:21:39 +0100

    Reach a first working example

--- create_example_data.py
+++ create_example_data.py
@@ -6,27 +6,72 @@ import zarr
 # v0.4 JSON Schema
 # https://github.com/ome/ngff/blob/main/0.4/schemas/image.schema
 
-# Invalid containers
-
-# 01: just no attributes
-image = zarr.open_group("data/invalid-image-01.zarr", "w")
+image = zarr.open_group("data/image-01.zarr", "w")
 image.attrs.put({})
-
-image = zarr.open_group("data/valid-image-01.zarr", "w")
-image.attrs.put({"version": "0.4", "multiscales": []})
+# -> fail
 
 image = zarr.open_group("data/valid-image-02.zarr", "w")
-image.attrs.put({"version": "0.4"})
+image.attrs.put({"version": "0.4", "multiscales": []})
 labels = image.create_group("labels")
 labels.attrs.put({"labels": []})
+# -> fail, with no obvious error (hanging, with "loading" message)
 
-"""
-subgroup = root.create_group("my_subgroup")
-subgroup.attrs.put({"key2": "value2"})
+image = zarr.open_group("data/valid-image-03.zarr", "w")
+image.attrs.put(
+    {
+        "version": "0.4",
+        "multiscales": [
+            {
+                "axes": [
+                    {"name": "y", "type": "space", "unit": "micrometer"},
+                    {"name": "x", "type": "space", "unit": "micrometer"},
+                ],
+                "datasets": [
+                    {
+                        "coordinateTransformations": [
+                            {"scale": [1.0, 1.0], "type": "scale"}
+                        ],
+                        "path": "0",
+                    },
+                ],
+                "version": "0.4",
+            }
+        ],
+    }
+)
+labels = image.create_group("labels")
+labels.attrs.put({"labels": []})
+# -> fails correctly, because of missing .zarray
 
+image = zarr.open_group("data/valid-image-04.zarr", "w")
+image.attrs.put(
+    {
+        "version": "0.4",
+        "multiscales": [
+            {
+                "axes": [
+                    {"name": "y", "type": "space", "unit": "micrometer"},
+                    {"name": "x", "type": "space", "unit": "micrometer"},
+                ],
+                "datasets": [
+                    {
+                        "coordinateTransformations": [
+                            {"scale": [1.0, 1.0], "type": "scale"}
+                        ],
+                        "path": "0",
+                    },
+                ],
+                "version": "0.4",
+            }
+        ],
+    }
+)
+labels = image.create_group("labels")
+labels.attrs.put({"labels": []})
 zarr.open_array(
-    "my_root.zarr/my_subgroup/my_array",
+    "data/valid-image-04.zarr/0",
+    dimension_separator="/",
     shape=(1000, 1000),
     chunks=(100, 100),
 )
-"""
+# succeeds
